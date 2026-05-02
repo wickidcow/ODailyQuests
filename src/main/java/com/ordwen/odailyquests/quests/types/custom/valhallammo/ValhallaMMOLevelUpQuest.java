4b01380e9d38c2e70d4007908983f086216a65cf
wickidcow <jaywago@gmail.com>
2026-05-02 18:40:09 -0400

    Add ValhallaMMOLevelUpQuest class

---
+++ src/main/java/com/ordwen/odailyquests/quests/types/custom/valhallammo/ValhallaMMOLevelUpQuest.java
@@ -0,0 +1,10 @@
+package com.ordwen.odailyquests.quests.types.custom.valhallammo;
+
+import com.ordwen.odailyquests.quests.types.shared.BasicQuest;
+
+public class ValhallaMMOLevelUpQuest extends ValhallaMMOQuest {
+
+    public ValhallaMMOLevelUpQuest(final BasicQuest basicQuest) {
+        super(basicQuest, "VALHALLA_LEVEL_UP");
+    }
+}
