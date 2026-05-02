18fa49d3031577caa55ae0fae11523ea7f4d198a
wickidcow <jaywago@gmail.com>
2026-05-02 18:39:43 -0400

    Add ValhallaMMOExpQuest class for experience quests

---
+++ src/main/java/com/ordwen/odailyquests/quests/types/custom/valhallammo/ValhallaMMOExpQuest.java
@@ -0,0 +1,10 @@
+package com.ordwen.odailyquests.quests.types.custom.valhallammo;
+
+import com.ordwen.odailyquests.quests.types.shared.BasicQuest;
+
+public class ValhallaMMOExpQuest extends ValhallaMMOQuest {
+
+    public ValhallaMMOExpQuest(final BasicQuest basicQuest) {
+        super(basicQuest, "VALHALLA_EXP");
+    }
+}
