557a1c7e47839faec49e23b6644a18240a4c023d
wickidcow <jaywago@gmail.com>
2026-08-17 13:30:46 -0400

    Validate Slimefun item quest IDs

--- src/main/java/com/ordwen/odailyquests/quests/types/global/CustomQuest.java
+++ src/main/java/com/ordwen/odailyquests/quests/types/global/CustomQuest.java
@@ -1,11 +1,18 @@
 package com.ordwen.odailyquests.quests.types.global;
 
+import com.ordwen.odailyquests.events.listeners.integrations.slimefun.SlimefunIntegration;
+import com.ordwen.odailyquests.quests.features.QuestFeatures;
 import com.ordwen.odailyquests.quests.player.progression.Progression;
 import com.ordwen.odailyquests.quests.types.AbstractQuest;
 import com.ordwen.odailyquests.quests.types.shared.BasicQuest;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.event.Event;
+import org.bukkit.event.entity.EntityPickupItemEvent;
+import org.bukkit.event.inventory.CraftItemEvent;
+import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class CustomQuest extends AbstractQuest {
 
@@ -20,12 +27,21 @@ public class CustomQuest extends AbstractQuest {
 
     @Override
     public boolean canProgress(@Nullable Event provided, Progression progression) {
-        return true;
+        if (!"SLIMEFUN_ITEM".equalsIgnoreCase(getQuestType())) return true;
+        if (provided == null) return false;
+
+        ItemStack stack = null;
+        if (provided instanceof EntityPickupItemEvent pickup) stack = pickup.getItem().getItemStack();
+        else if (provided instanceof CraftItemEvent craft) stack = craft.getCurrentItem();
+
+        String id = SlimefunIntegration.getItemId(stack);
+        if (id == null) return false;
+        List<String> requiredIds = QuestFeatures.slimefunIds(this);
+        return requiredIds.isEmpty() || requiredIds.contains(id);
     }
 
     @Override
     public boolean loadParameters(ConfigurationSection section, String file, String index) {
         return true;
     }
-
 }
