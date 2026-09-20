11c4c1dd4d0eb5bd56baef77c2a9ddcf5f39d6b7
wickidcow <jaywago@gmail.com>
2026-09-20 10:10:51 -0400

    Fix ENCHANT quest event handling

--- src/main/java/com/ordwen/odailyquests/events/listeners/item/EnchantItemListener.java
+++ src/main/java/com/ordwen/odailyquests/events/listeners/item/EnchantItemListener.java
@@ -1,7 +1,6 @@
 package com.ordwen.odailyquests.events.listeners.item;
 
 import com.ordwen.odailyquests.configuration.essentials.Debugger;
-
 import com.ordwen.odailyquests.quests.player.progression.PlayerProgressor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -10,11 +9,18 @@ import org.bukkit.event.enchantment.EnchantItemEvent;
 
 public class EnchantItemListener extends PlayerProgressor implements Listener {
 
-    @EventHandler(priority = EventPriority.HIGHEST)
+    /**
+     * Progress ENCHANT quests only after every normal plugin handler has had a chance
+     * to accept, cancel, or un-cancel the enchant.
+     *
+     * <p>Using MONITOR with ignoreCancelled avoids an ordering race at HIGHEST where
+     * ODailyQuests could observe the event as cancelled even though a later handler
+     * allowed the enchant to complete.</p>
+     */
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
     public void onEnchantItemEvent(EnchantItemEvent event) {
-        if (event.isCancelled()) return;
-
-        Debugger.write("EnchantItemListener: onEnchantItemEvent summoned by " + event.getEnchanter().getName() + " for " + event.getItem().getType() + ".");
+        Debugger.write("EnchantItemListener: successful enchant by "
+                + event.getEnchanter().getName() + " for " + event.getItem().getType() + ".");
         setPlayerQuestProgression(event, event.getEnchanter(), 1, "ENCHANT");
     }
 }
